ceb1bf7f7500990cc4fcb4f5a526dbec2b15b606
mrkotyaka <mrkotyaka@gmail.com>
2025-06-03 22:29:16 +0300

    Initial commit

---
+++ src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
@@ -0,0 +1,45 @@
+package ru.netology.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class PostRepositoryStubImpl implements PostRepository {
+  private final Map<Long, Post> postMap;
+  private final AtomicLong postIds;
+
+  public PostRepositoryStubImpl() {
+    postMap = new ConcurrentHashMap<>();
+    postIds = new AtomicLong(1);
+  }
+  public List<Post> all() {
+    return new ArrayList<>(postMap.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(postMap.get(id));
+  }
+
+  public Post save(Post post) {
+    if (post.getId() == 0) {
+      while (postMap.containsKey(postIds.longValue())) postIds.getAndIncrement();
+      post.setId(postIds.getAndIncrement());
+    }
+
+    postMap.put(post.getId(), post);
+    return post;
+  }
+
+  public void removeById(long id) {
+    if (postMap.containsKey(id)) {
+      postMap.remove(id);
+    } else {
+      throw new NotFoundException("Post with id " + id + " not found");
+    }
+  }
+}
